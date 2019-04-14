64dae1e9d2c587628a892f62f48705f87767ffbc
Uanderson Nunes <uandersonmbc@hotmail.com>
2019-04-13 21:00:30 -0300

    Subindo questões feitas

--- 01-Lista/todas.hs
+++ 01-Lista/todas.hs
@@ -1,8 +1,8 @@
 -- 02 menor entre x y
-menor x y = min x y
+menord2 x y = min x y
 
 -- 02 menor entre x y z
-menor x y z = min z (min x y)
+menord3 x y z = min z (min x y)
 
 -- 03 fatorial
 fat x = product [1 .. x]
@@ -14,3 +14,15 @@ fib 1 = 0
 fib 2 = 1
 fib x = fib (x - 1) + fib (x - 2)
 
+-- 05 elemento
+elemento x xs = xs !! x
+
+-- 06 pertence 
+pertence x xs = x `elem` xs
+
+-- 07 total
+total [] = 0
+total [x] = 1
+total xs = total (tail xs) + 1
+
+-- 08 frequencia
